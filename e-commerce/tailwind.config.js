ccf7b8cb2459c3a049a11f79e50804a5ad1814a8
Sahatsawat-Wattana <sahatsawat.wattanapraditchai@gmail.com>
2026-04-01 18:55:48 +0700

    commit ux/ui business

---
+++ e-commerce/tailwind.config.js
@@ -0,0 +1,11 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        caslon: ['"Libre Caslon Display"', "serif"],
+      },
+    },
+  },
+};
